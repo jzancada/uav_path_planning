b50b42467fe42783462769636aff4e6ef797729c
jzancada <jzancada@indra.es>
2018-11-30 00:45:10 +0100

    resultados control_x_1

--- uav_z_2.py
+++ uav_z_2.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# tiempo de muestreo [segundos]
 Ts = 0.2
 
 class uav_class:
@@ -9,20 +10,34 @@ class uav_class:
     def __init__(self, x0 = 0, x0_dot = 0):
         self.x = x0
         self.x_dot = x0_dot
-        self.u_lim = 1
+        self.u_lim = .4
+        self.v_lim = 2.
+        self.x0 = 1.
+        self.N = 2 # divide u (retrasa control aceleracion)
+        self.K = np.sqrt(self.u_lim / (2.0*self.x0))
+        self.x0_dot_lim = np.sqrt(2.0*self.x0*self.u_lim)
 
+    # siguiente muestra
     def next(self, u, Ts):
         self.x = self.x + self.x_dot*Ts
         self.x_dot = self.x_dot + u*Ts; 
 
+    # limita la aceleracion (entada)
     def limita_u(self, u):
         uOut = u
         if abs(uOut)>self.u_lim:
-            uOut = np.sign(uOut)*self.u_lim
-            
+            uOut = np.sign(uOut)*self.u_lim            
         return uOut
 
-    def control_x(self, x_req, K, Ts):
+    # limita la velocidad 
+    def limita_v(self, v):
+        vOut = v
+        if abs(vOut)>self.v_lim:
+            vOut = np.sign(vOut)*self.v_lim            
+        return vOut
+
+    # control de posicion
+    def control_x_0(self, x_req, K, Ts):
         u = 0.0
         e_x = self.x - x_req
         uTs = -self.x_dot -K*(e_x + self.x_dot*Ts) 
@@ -30,29 +45,55 @@ class uav_class:
         u = self.limita_u(u)
         return u
 
+    def control_x_1(self, x_req, Ts):
+        e_x = self.x - x_req
+
+        if self.x > 2*self.x0:
+            v_next = -np.sqrt( (self.x - self.x0)*2*self.u_lim)
+        elif self.x < -2*self.x0:
+            v_next =  np.sqrt(-(self.x + self.x0)*2*self.u_lim)
+        else:
+            v_next = -self.K*e_x
+
+        v_next = self.limita_v(v_next)
+        # v_next = v + u*Ts
+
+        u = (v_next - self.x_dot)/Ts
+        ## retrasa u
+        u = u / self.N
+        u = self.limita_u(u)
+        return u
+
     def disp(self):
         print 'x=%.02f' % self.x, ', x_dot=%.02f' % self.x_dot
 
 
-
-uav = uav_class(1.0, 0)
+# se crea uav (de)
+# posicion inicial
+x_ini = 20
+# posicion inicial
+x_dot_ini = 0
+uav = uav_class(x_ini, x_dot_ini)
 
 list_x=[]
 list_x_dot=[]
 list_u=[]
 # Cte de control
-K = 4
+print ("===============")
+print ("K         = %.3f" % uav.K)
+print ("x_dot lim = %.3f" % uav.x0_dot_lim)
+print ("===============")
+
 # posicion requerida
 x_req = 0 
-for k in range(40):
-    uav.disp()
-    u = uav.control_x(x_req, K, Ts)
+for k in range(100):
+#    uav.disp()
+    u = uav.control_x_1(x_req, Ts)
     uav.next(u, Ts)
     list_x.append(uav.x)
     list_x_dot.append(uav.x_dot)
     list_u.append(u)
 
-print list_u
 
 # posicion    
 plt.plot(list_x)
@@ -84,6 +125,14 @@ plt.grid()
 # plt.xlim([0, 1])
 plt.draw()
 plt.savefig('test_3.png')
+
+# plano x -x_dot
+plt.figure()
+plt.plot(list_x, list_x_dot)
+plt.draw()
+plt.xlabel('x')
+plt.ylabel('x_dot')
+plt.savefig('test_4.png')
 
 plt.show()
 
